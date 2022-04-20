33fb6ca8325103542d5f5ceb59c0a97eb090f9da
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-04-21 00:10:37 +0100

    File  name updated

---
+++ java/src/creed/Perlin_noise.java
@@ -0,0 +1,106 @@
+package team_Aayush;
+
+import ddf.minim.*;
+import ddf.minim.analysis.*;
+import ddf.minim.effects.*;
+import ddf.minim.signals.*;
+import ddf.minim.spi.*;
+import ddf.minim.ugens.*;
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Perlin_noise extends PApplet {
+
+    // Configuration variables
+    // ------------------------
+    int canvasWidth = 1080;
+    int canvasHeight = 1080;
+
+    String audioFileName = "[YT2mp3.info] - Assassin_'s Creed II   Ezio_'s Family (Dubstep Remix) Remake (320kbps)"; // Audio
+                                                                                                                     // file
+                                                                                                                     // in
+                                                                                                                     // data
+                                                                                                                     // folder
+
+    float fps = 30;
+    float smoothingFactor = (float) 0.25; // FFT audio analysis smoothing factor
+    // ------------------------
+
+    // Global variables
+    AudioPlayer track;
+    FFT fft;
+    Minim minim;
+
+    // General
+    int bands = 256; // must be multiple of two
+    float[] spectrum = new float[bands];
+    float[] sum = new float[bands];
+
+    // Graphics
+    float unit;
+    int groundLineY;
+    PVector center;
+
+    public void settings() {
+        size(canvasWidth, canvasHeight);
+        smooth(8);
+    }
+
+    public void setup() {
+
+        frameRate(fps);
+
+        // Graphics related variable setting
+        unit = height / 100; // Everything else can be based around unit to make it change depending on size
+        strokeWeight(unit / 10.24f);
+        groundLineY = height * 3 / 4;
+        center = new PVector(width / 2, height * 3 / 4);
+
+        minim = new Minim(this);
+        track = minim.loadFile(audioFileName, 2048);
+
+        track.loop();
+
+        fft = new FFT(track.bufferSize(), track.sampleRate());
+
+        fft.linAverages(bands);
+
+        // track.cue(60000); // Cue in milliseconds
+    }
+
+    int sphereRadius;
+
+    float spherePrevX;
+    float spherePrevY;
+
+    int yOffset;
+
+    boolean initialStatic = true;
+    float[] extendingSphereLinesRadius;
+
+    void drawStatic() {
+
+        if (initialStatic) {
+            extendingSphereLinesRadius = new float[241];
+
+            for (int angle = 0; angle <= 240; angle += 4) {
+                extendingSphereLinesRadius[angle] = map(random(1), 0, 1, sphereRadius, sphereRadius * 7);
+            }
+
+            initialStatic = false;
+        }
+    // More extending lines
+  for (int angle = 0; angle <= 240; angle += 4) {
+
+    float x = round(cos(radians(angle + 150)) * sphereRadius + center.x);
+    float y = round(sin(radians(angle + 150)) * sphereRadius + groundLineY - yOffset);
+ 
+    float xDestination = x;
+    float yDestination = y;    
+
+    }
+
+    
+
+}
+}
