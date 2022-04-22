ffb23fbf485b1bb0d18881b5bf46daff1740d4de
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-04-22 11:41:17 +0100

    Circle pattern file deleted

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -15,11 +15,6 @@ public class Main {
 		processing.core.PApplet.runSketch(a, new test_audio_1());
 	}
 
-	public void Circle_Pattern() {
-		String[] a = { "MAIN" };
-		processing.core.PApplet.runSketch(a, new test_audio_circle_pattern());
-	}
-
 	public void radioActive() {
 		String[] a = { "MAIN" };
 		processing.core.PApplet.runSketch(a, new radiohead_visual());
@@ -59,12 +54,11 @@ public class Main {
 		Main main = new Main();
 		// main.startUI();
 		// main.Circle_Effect();
-		// main.Circle_Pattern();
 		// main.radioActive();
 		// main.recordPlayer();
 		// main.musicViz();
-		// main.LuYu();
-		main.MeshLine();
+		main.LuYu();
+		// main.MeshLine();
 		// main.flower();
 		// main.osclliopscope();
 
