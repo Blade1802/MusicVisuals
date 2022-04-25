7af82d2fec9e628e582644d6ba12cf5172d765f9
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-04-25 10:00:23 +0100

    switch_menu updated

--- java/src/creed/switch_menu.java
+++ java/src/creed/switch_menu.java
@@ -81,6 +81,7 @@ public class switch_menu extends PApplet {
     public void settings() {
         size(1024, 600, P3D);
         // fullScreen(P3D, SPAN);
+        smooth();
     }
 
     public void setup() {
@@ -116,6 +117,16 @@ public class switch_menu extends PApplet {
 
     // case 2 - Mesh Line with tape recorder
     float off = 0;
+
+    // case 3
+    // LuYu effect
+    int n = 0;
+    float c1 = 51;
+    float c2 = 204;
+    float c3 = 255;
+
+    // for white
+    float w1 = 255;
 
     public void draw() {
         // using cases
@@ -296,6 +307,34 @@ public class switch_menu extends PApplet {
                 break;
 
             }
+            case 3: {
+                // LuYu effect
+                background(0);
+                translate(width / 2, height / 2);
+                for (int i = 0; i < ap.bufferSize() - 1; i++) {
+                    rotateX((float) (n * -PI / 4 * 0.05));
+                    // rotateY(n*-PI/3*0.05);
+                    // fill(255, 150);
+                    // fill(255, 105, 180);// hot pink
+                    fill(255, 20, 147);// dark pink
+                    // fill(random(w1++ % 255), random(w1++ % 255));
+
+                    float leftLevel = ap.left.level() * 100;
+                    ellipse(i, i, leftLevel, leftLevel);
+                    rotateZ((float) (n * -PI / 5 * 0.05)); // 3 -> 1
+                    // fill(0, 150);
+                    // fill(255, 0, 69);
+                    // fill(34, 139, 34);// green
+                    fill(124, 252, 0);// lawn green
+                    // fill(255, 140, 0);
+
+                    float rightLevel = ap.right.level() * 100;
+                    rect(i, i, rightLevel, rightLevel);
+                }
+                n++;
+
+            }
+                break;
 
         }// end switch case
     }// end draw()
@@ -307,4 +346,10 @@ public class switch_menu extends PApplet {
         text((int) (time / 1000 - millis() / 1000) / 60 + ":" + (time / 1000 - millis() / 1000) % 60, -7, 21);
     }
 
-}
+    public void stop() {
+        ap.close();
+        minim.stop();
+        super.stop();
+    }
+
+}//end class switch_menu
