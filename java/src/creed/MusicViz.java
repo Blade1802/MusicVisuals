62f5bc0ee8f69ef2f7272f3b17c9351ff5b54e7d
Ayan Abedin <abedin.ayanJARVIS@yahoo.com>
2022-05-02 03:08:51 +0100

    Music Viz updated with map in outer circle

--- java/src/creed/MusicViz.java
+++ java/src/creed/MusicViz.java
@@ -8,6 +8,7 @@ import ddf.minim.Minim;
 import ddf.minim.analysis.*;
 import ddf.minim.*;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 
 public class MusicViz extends PApplet {
@@ -16,17 +17,20 @@ public class MusicViz extends PApplet {
     AudioPlayer ap;
     AudioMetaData meta;
     BeatDetect beat;
-    int r = 300;
+    int r = 200;
     float rad = 0;
 
+    // Image
+    PImage bg;
 
     public void settings() {
-        // size(800, 600, P3D);
-        fullScreen(P3D, SPAN);
+        size(800, 600, P3D);
     }
 
     public void setup() {
 
+        // using background image
+        bg = loadImage("assassins creed.jpg");
 
         minim = new Minim(this);
         ap = minim.loadFile(
@@ -43,6 +47,7 @@ public class MusicViz extends PApplet {
 
     public void draw() {
         background(0);
+        colorMode(HSB);
         // float t = map(mouseX, 0, width, 0, 1);
         beat.detect(ap.mix);
         // fill(26, 31, 24);
@@ -54,9 +59,9 @@ public class MusicViz extends PApplet {
         noFill();
         // fill(-1, 10);
         // fill(random(255), random(255), random(255));
-        fill(255, 255, 255);
+        fill(255);
         if (beat.isOnset())
-            rad = (float) (rad * 0.9);
+            rad = (float) (rad * 0.8);
         else
             rad = 70;
         ellipse(0, 0, 2 * rad, 2 * rad);
@@ -64,19 +69,20 @@ public class MusicViz extends PApplet {
         int bsize = ap.bufferSize();
 
         for (int i = 0; i < bsize - 1; i += 5) {
-            float x = (r) * cos(i * 3 * PI / bsize);
-            float y = (r) * sin(i * 3 * PI / bsize);
-            float x2 = (r + ap.left.get(i) * 100) * cos(i * 3 * PI / bsize);
-            float y2 = (r + ap.left.get(i) * 100) * sin(i * 3 * PI / bsize);
+            stroke(map(i, 0, bsize - 1, 0, 255), 255, 255);
+            float x = (r) * cos(i * 2 * PI / bsize);
+            float y = (r) * sin(i * 2 * PI / bsize);
+            float x2 = (r + ap.left.get(i) * 100) * cos(i * 2 * PI / bsize);
+            float y2 = (r + ap.left.get(i) * 100) * sin(i * 2 * PI / bsize);
             line(x, y, x2, y2);
         }
 
         beginShape();
         noFill();
         stroke(-1, 50);
-        for (int i = 0; i < bsize; i += 60) {
-            float x2 = (r + ap.left.get(i) * 100) * cos(i * 4 * PI / bsize);
-            float y2 = (r + ap.left.get(i) * 100) * sin(i * 4 * PI / bsize);
+        for (int i = 0; i < bsize; i += 30) {
+            float x2 = (r + ap.left.get(i) * 100) * cos(i * 2 * PI / bsize);
+            float y2 = (r + ap.left.get(i) * 100) * sin(i * 2 * PI / bsize);
             vertex(x2, y2);
             pushStyle();
             stroke(-1);
